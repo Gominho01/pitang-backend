5a7887ddb663cfd54399e3167ac8eab2f7342e4d
LucasGominho <lcpg@cesar.school>
2024-07-13 02:10:47 -0300

    feat: adding appointmentDay to create

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -5,15 +5,17 @@ const getAll = (req, res) => {
 };
 
 const createAppointment = (req, res) => {
-	const { name, date } = req.body;
-	if (!name || !date) {
-		return res.status(400).json({ error: 'Name and date are required' });
-	}
-	const newAppointment = {
-			id: Date.now().toString(),
-			name,
-			date
-	};
-			appointments.push(newAppointment);
+  const { name, birthDate, appointmentDate } = req.body;
+  if (!name || !birthDate || !appointmentDate) {
+    return res.status(400).json({ error: 'Name, birthDate, and appointmentDate are required' });
+  }
+  const newAppointment = {
+    id: Date.now().toString(),
+    name,
+    birthDate,
+    appointmentDate
+  };
+
+  appointments.push(newAppointment);
 			res.status(201).json(newAppointment);
 };
