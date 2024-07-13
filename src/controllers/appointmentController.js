36fc0506b7a67494cd547c4bcea1a74b3fd6596e
LucasGominho <lcpg@cesar.school>
2024-07-13 02:12:25 -0300

    feat: limiting appointment per hour

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -1,4 +1,5 @@
-let appointments = [];
+const appointments = [];
+const MAX_APPOINTMENTS_PER_HOUR = 2;
 
 const getAll = (req, res) => {
   res.json(appointments);
@@ -6,6 +7,15 @@ const getAll = (req, res) => {
 
 const createAppointment = (req, res) => {
   const { name, birthDate, appointmentDate } = req.body;
+
+  const appointmentsAtSameTime = appointments.filter(
+    app => app.appointmentDate === appointmentDate
+  );
+
+  if (appointmentsAtSameTime.length >= MAX_APPOINTMENTS_PER_HOUR) {
+    return res.status(400).json({ error: 'Este horário já está totalmente ocupado' });
+  }
+
   if (!name || !birthDate || !appointmentDate) {
     return res.status(400).json({ error: 'Name, birthDate, and appointmentDate are required' });
   }
