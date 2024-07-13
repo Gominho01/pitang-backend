dfaf1d5ecfd587111e9235ff6f8d61d380c2297d
LucasGominho <lcpg@cesar.school>
2024-07-13 02:52:27 -0300

    feat: creating router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const appointmentRoutes = require('./routes/route');
+const appointmentRoutes = require('./routes/router');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
