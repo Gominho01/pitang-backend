8723279fd674d5a080d53b8d9697030afb86c72e
LucasGominho <lcpg@cesar.school>
2024-07-13 02:08:50 -0300

    feat: adding createAppointment

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -3,3 +3,17 @@ let appointments = [];
 const getAll = (req, res) => {
   res.json(appointments);
 };
+
+const createAppointment = (req, res) => {
+	const { name, date } = req.body;
+	if (!name || !date) {
+		return res.status(400).json({ error: 'Name and date are required' });
+	}
+	const newAppointment = {
+			id: Date.now().toString(),
+			name,
+			date
+	};
+			appointments.push(newAppointment);
+			res.status(201).json(newAppointment);
+};
