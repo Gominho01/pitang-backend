a7da31cddc1c5d40973443afaf19fa2c7ac80c6e
LucasGominho <lcpg@cesar.school>
2024-07-13 23:37:23 -0300

    chore: convert router to TypeScript

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,5 @@
-const express = require('express');
-const { getAll, createAppointment, deleteAppointment, getOne, updateStatus} = require('../controllers/appointmentController');
+import express from 'express';
+import { getAll, createAppointment, deleteAppointment, getOne, updateStatus} from '../controllers/appointmentController';
 
 const router = express.Router();
 
@@ -9,4 +9,4 @@ router.delete('/appointments/:id', deleteAppointment);
 router.patch('/appointments/:id', updateStatus);
 router.post('/appointments', createAppointment);
 
-module.exports = router;
+export default router;
