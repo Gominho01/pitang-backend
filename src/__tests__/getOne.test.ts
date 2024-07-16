28d38bb84b905ad641bf394ca79cfba89e1a28cf
LucasGominho <lcpg@cesar.school>
2024-07-16 18:36:31 -0300

    test: should get specific id

--- src/__tests__/getOne.test.ts
+++ src/__tests__/getOne.test.ts
@@ -11,3 +11,18 @@ beforeAll((done) => {
 afterAll((done) => {
   server.close(done);
 });
+
+describe('Appointment Controller - Get One Appointment', () => {
+  it('should get a specific appointment by ID', async () => {
+    const createRes = await request(server)
+      .post('/api/appointments')
+      .send(validAppointmentMock);
+    const { id } = createRes.body;
+
+    const res = await request(server)
+      .get(`/api/appointments/${id}`);
+
+    expect(res.body).toHaveProperty('id', id);
+    expect(res.body.name).toBe(validAppointmentMock.name); 
+  }, 10000);
+});
