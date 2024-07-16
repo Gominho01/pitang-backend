1a83b4fb5e87ab84147a03358d5769757c996370
LucasGominho <lcpg@cesar.school>
2024-07-16 14:13:35 -0300

    feat: adding invalid appointment day mock

---
+++ src/__tests__/mocks/invalidAppointmentDayMock.ts
@@ -0,0 +1,7 @@
+const invalidAppointmentDayMock = {
+    name: 'Lucas Cortes',
+    birthDate: '1990-01-01',
+    appointmentDay: '2000-01-01',
+    completed: false,
+    conclusion: 'Initial check-up',
+};
