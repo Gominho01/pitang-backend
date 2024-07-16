bb13b4f2bae24d56b95bbc2aa54729a919b56e91
LucasGominho <lcpg@cesar.school>
2024-07-16 14:14:25 -0300

    feat: adding valid mock

---
+++ src/__tests__/mocks/validAppointmentMock.ts
@@ -0,0 +1,7 @@
+export const validAppointmentMock = {
+    name: 'Alice Smith',
+    birthDate: '1990-01-01',
+    appointmentDay: new Date().toISOString().split('T')[0],
+    completed: false,
+    conclusion: 'Initial check-up',
+};
