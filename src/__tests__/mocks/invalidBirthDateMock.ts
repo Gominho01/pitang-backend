02788eec26072f206e332a48608bf122fc89c70f
LucasGominho <lcpg@cesar.school>
2024-07-16 14:14:01 -0300

    feat: adding invalid birthday mock

---
+++ src/__tests__/mocks/invalidBirthDateMock.ts
@@ -0,0 +1,7 @@
+export const invalidBirthDateMock = {
+    name: 'Victor valdes',
+    birthDate: '3000-01-01',
+    appointmentDay: new Date().toISOString().split('T')[0],
+    completed: false,
+    conclusion: 'Initial check-up',
+};
