8185ef434935c6b2b87059bdfd85c95fc972e2d3
LucasGominho <lcpg@cesar.school>
2024-07-16 19:45:54 -0300

    test: update appointement status

--- src/__tests__/update.test.ts
+++ src/__tests__/update.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import app from '../index';
+import { validAppointmentMock } from '../mocks/validAppointmentMock';
 
 let server: any;
 
@@ -9,4 +10,21 @@ beforeAll((done) => {
 
 afterAll((done) => {
   server.close(done);
+});
+
+describe('Appointment Controller - Update Appointment', () => {
+  it('should update the status of an appointment', async () => {
+
+    const createRes = await request(server)
+      .post('/api/appointments')
+      .send( validAppointmentMock );
+    const { id } = createRes.body;
+
+    const updateRes = await request(server)
+      .patch(`/api/appointments/${id}`)
+      .send({ completed: true, conclusion: 'Done' });
+    expect(updateRes.status).toBe(200);
+    expect(updateRes.body).toHaveProperty('completed', true);
+    expect(updateRes.body).toHaveProperty('conclusion', 'Done');
+  }, 10000);
 });
